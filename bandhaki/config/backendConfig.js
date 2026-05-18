740995d77f395acbc149601b54291e00cd54102a
Ankit Khanal <akhanal749@gmail.com>
2026-05-18 11:09:05 +0545

    chore: update comment for backend URL change instructions

--- bandhaki/config/backendConfig.js
+++ bandhaki/config/backendConfig.js
@@ -2,5 +2,5 @@ const BACKEND_URL = 'http://192.168.1.88:5000';
 
 export default BACKEND_URL;
 
-
+// Change backend URL and perform below command to change backend URL for app
 // cd bandhaki && eas update --branch preview --platform android --message "Update backend URL"
